1b797fe52f4dfdd58055e31ffb1d2201d830e96d
Project Generator <generator@nextnode.dev>
2025-09-15 19:58:07 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * 
+ * {{project_description}}
+ */
+
+// Main library exports
+export * from './lib/core.js'
+
+// Type exports
+export type * from './types/index.js'
+
+// Utility exports
+export * from './utils/index.js'
